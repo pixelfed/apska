eacaef1538e1498bed4eaf529c6b1d75bf2d5074
Daniel Supernault <danielsupernault@gmail.com>
2024-11-27 01:19:32 -0700

    Update user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('domain')->nullable()->index();
+            $table->unsignedInteger('instance_id')->nullable()->index();
+            $table->string('acct')->unique()->index();
+            $table->string('username')->nullable();
+            $table->unsignedTinyInteger('status')->default(2);
+            $table->unsignedTinyInteger('role')->default(5);
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->text('bearer_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
